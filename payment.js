efbe6aa86fc61271a7e794cf06ee83b8ef52cb0c
test <test@corelogic.com>
2018-12-07 07:28:14 -0600

    Update script

--- payment.js
+++ payment.js
@@ -1,3 +1,13 @@
 window['Renttrack'] = function () {
-	return 'Hello RentTrack';
+	return {
+		create: function () {
+			return '<p>Credit or debit card number</p>' +
+				'<p>Credit or debit card expiration date</p>' +
+				'<p>Credit or debit card CVC/CVV</p>'
+		},
+		createToken: function () {
+			console.log('Request authorization id ...');
+			return 'random_authorization_id_123';
+		}
+	};
 };
