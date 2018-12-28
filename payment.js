ca7614bf36de19b48f347b13107a42f923e3f514
test <test@corelogic.com>
2018-12-28 07:04:23 -0800

    Change html structure

--- payment.js
+++ payment.js
@@ -2,14 +2,9 @@ window['Renttrack'] = function () {
 	return {
 		create: function (containerId) {
 			const containerHtml =
-				'<label for="card_number">Card Number</label>' +
-				'<input id="card_number">' +
-
-				'<label for="exp_date">Expiration Date</label>' +
-				'<input id="exp_date">' +
-
-				'<label for="cvv">CVV</label>' +
-				'<input id="cvv">';
+				'<label for="card_number">Card Number<input id="card_number"></label>' +
+				'<label for="exp_date">Expiration Date<input id="exp_date"></label>' +
+				'<label for="cvv">CVV<input id="cvv"></label>';
 
 			document.getElementById(containerId).innerHTML = containerHtml;
 		},
