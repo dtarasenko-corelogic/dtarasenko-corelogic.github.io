dd9a9b2530ba5578e337beb5ddd6b9bb579b975b
test <test@corelogic.com>
2018-12-28 05:14:23 -0800

    add htm to container'

--- payment.js
+++ payment.js
@@ -1,9 +1,10 @@
 window['Renttrack'] = function () {
 	return {
-		create: function () {
-			return '<p>Credit or debit card number</p>' +
+		create: function (containerId) {
+			const containerHtml = '<p>Credit or debit card number</p>' +
 				'<p>Credit or debit card expiration date</p>' +
-				'<p>Credit or debit card CVC/CVV</p>'
+				'<p>Credit or debit card CVC/CVV</p>';
+			document.getElementById(containerId).innerHTML = containerHtml;
 		},
 		createToken: function () {
 			console.log('Request authorization id ...');
