e011676d07ad9aba40c860a56192d791af346587
test <test@corelogic.com>
2019-01-02 07:08:56 -0800

    Add parameter to createToken function and extract card from html

--- payment.js
+++ payment.js
@@ -2,22 +2,27 @@ window['Renttrack'] = function () {
 	return {
 		create: function (containerId) {
 			const containerHtml =
-				'<label for="card_number">Card Number<input id="card_number"></label>' +
-				'<label for="exp_date">Expiration Date<input id="exp_date"></label>' +
-				'<label for="cvv">CVV<input id="cvv"></label>';
+				'<label for="card_number">Card Number<input id="renttrack_card_number"></label>' +
+				'<label for="exp_date">Expiration Date<input id="renttrack_exp_date"></label>' +
+				'<label for="cvv">CVV<input id="renttrack_cvv"></label>';
 
 			document.getElementById(containerId).innerHTML = containerHtml;
 		},
-		createToken: function () {
+		createToken: function (additionalInfo) {
 			console.log('Request authorization id ...');
 
-			const response = {
-				status: 'pending',
-				authorization_id: '123_456_789'
+			const card = {
+				cardNumber: document.getElementById('renttrack_card_number').value,
+				expDate: document.getElementById('renttrack_exp_date').value,
+				cvv: document.getElementById('renttrack_cvv').value
 			};
 
 			return new Promise(function (resolve, reject) {
-				setTimeout(function() {
+				setTimeout(function () {
+					const response = {
+						status: 'pending',
+						authorization_id: '123_456_789'
+					};
 					resolve(response, 1000);
 				});
 			});
